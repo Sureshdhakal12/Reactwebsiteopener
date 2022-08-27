9f09f245404e3543c3f870d83ca748db95dc2c9e
suresh dhakal <dhakalsuresh52@outlook.com>
2022-08-27 13:52:22 +0545

    :tada: Give heading and some style

---
+++ src/index.js
@@ -0,0 +1,11 @@
+ import React from "react";
+ import  ReactDOM  from "react-dom";
+ import './style.css'
+
+ function App(){
+    return <div><h1>Website opener</h1>
+    <h3>Which site You like to...??</h3>
+    </div>
+ }
+
+ ReactDOM.render(<App/>,document.getElementById("root"))
