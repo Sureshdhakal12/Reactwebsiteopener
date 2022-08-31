171422eca7cc323f66dab504a315fc0a71a7b7c7
suresh dhakal <dhakalsuresh52@outlook.com>
2022-08-31 12:07:05 +0545

    :tada: Give heading and some style

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App= () =>{
+    return 
+    <div><h1>Website opener</h1>
+    <h3>Which site You like to...??</h3>
+    <button className="button">Google</button>
+    </div>
+ }
+
+ export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
  import React from "react";
  import  ReactDOM  from "react-dom";
  import './style.css'
-
- function App(){
-    return <div><h1>Website opener</h1>
-    <h3>Which site You like to...??</h3>
-    </div>
- }
+import App from './App'
+ 
 
  ReactDOM.render(<App/>,document.getElementById("root"))
