fa5a62879c2b9750574799852bdd6193452656d4
suresh dhakal <dhakalsuresh52@outlook.com>
2022-09-03 15:53:54 +0545

    :tada: creating style and app and import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
- import React from "react";
- import  ReactDOM  from "react-dom";
- import './style.css'
-import App from './App'
- 
+import React from "react";
+import ReactDOM  from "react";
+import './style.css'
+import App from "./App";
 
- ReactDOM.render(<App/>,document.getElementById("root"))
+
+
+ReactDOM.render(<App/>,document.getElementById("root"));  
