d4bfbc9dcd7b971671896443a9e6f322beb71665
suresh dhakal <dhakalsuresh52@outlook.com>
2022-09-03 16:30:24 +0545

    :sparkles: complete website opener

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-const App= () =>{
-    return 
-    <div><h1>Website opener</h1>
-    <h3>Which site You like to...??</h3>
-    <button className="button">Google</button>
-    </div>
- }
+import button from "./button";
 
- export default App;
+
+const App= ()=> {
+  return <div>
+    <h1>Website Opener</h1>
+    <h3>Which website You like to See</h3>
+    <button name="Google" link="htps://google.com"/>4
+    <button name="facebook" link="htps://facebook.com"/>
+
+    <button name="youtube" link="htps://youtube.com"/>
+    <button name="github" link="htps://github.com"/>
+
+    <button/>
+
+
+  </div>
+}
+
+export default App;
